326457d9c4fe4d4a888ab1148841dd60eb924f43
Lee Sol <leeso4567@naver.com>
2022-12-23 23:06:23 +0900

    MemberService 로직 구현

---
+++ src/main/java/hello/memberservice/service/MemberService.java
@@ -0,0 +1,49 @@
+package hello.memberservice.service;
+
+import hello.memberservice.domain.Member;
+import hello.memberservice.repository.MemberRepository;
+import hello.memberservice.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원 X
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> { //result 에 값이 있으면 member 가 들어올텐데, 그걸 m 으로 해서 사용
+//            //Optional 로 객체를 감쌌기 때문에 가능한 메서드이다.
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+        validateDuplicatedMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) { //중복 회원 검증하는 메서드
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 한명 조회
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
