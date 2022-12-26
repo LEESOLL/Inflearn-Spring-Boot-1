04ba80fe2b738a7295b5988b6d1f7aa6a2a3c9db
Lee Sol <leeso4567@naver.com>
2022-12-26 12:52:02 +0900

    @Controller, @Service, @Repository 어노테이션 등록 -> 컴포넌트 스캔

--- src/main/java/hello/memberservice/controller/MemberController.java
+++ src/main/java/hello/memberservice/controller/MemberController.java
@@ -1,2 +1,16 @@
-package hello.memberservice.controller;public class MemberController {
+package hello.memberservice.controller;
+
+
+import hello.memberservice.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 }
--- src/main/java/hello/memberservice/service/MemberService.java
+++ src/main/java/hello/memberservice/service/MemberService.java
@@ -3,14 +3,18 @@ package hello.memberservice.service;
 import hello.memberservice.domain.Member;
 import hello.memberservice.repository.MemberRepository;
 import hello.memberservice.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
