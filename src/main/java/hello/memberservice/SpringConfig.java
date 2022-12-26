bc0e8a29dbe8e3d572eac702cc6d57755c137754
Lee Sol <leeso4567@naver.com>
2022-12-26 14:41:47 +0900

    @Configuration을 이용해서 직접 스프링 컨테이너에 빈 등록하기

---
+++ src/main/java/hello/memberservice/SpringConfig.java
@@ -0,0 +1,21 @@
+package hello.memberservice;
+
+import hello.memberservice.repository.MemberRepository;
+import hello.memberservice.repository.MemoryMemberRepository;
+import hello.memberservice.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
