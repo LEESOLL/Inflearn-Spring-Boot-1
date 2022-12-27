9926e243bc4d614a61f5373344fedf07b624bf8c
Lee Sol <leeso4567@naver.com>
2022-12-27 12:06:21 +0900

    jdbc로 DB 연동하기

--- src/main/java/hello/memberservice/controller/MemberController.java
+++ src/main/java/hello/memberservice/controller/MemberController.java
@@ -1,9 +1,15 @@
 package hello.memberservice.controller;
 
 
+import hello.memberservice.domain.Member;
 import hello.memberservice.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,4 +19,28 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) { //Post 요청으로 받은 name 이 MemberForm form 을 통해 들어옴
+        Member member = new Member();
+        member.setName(form.getName()); //form 에서 받아온 name 가져와서 memberName 설정해줌
+        memberService.join(member);
+
+        return "redirect:/"; //회원가입 완료 후 홈 화면으로 보냄
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+
+    }
+
+
 }
