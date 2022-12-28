955355bcf926c488cf9b266965e84a19ffb9c914
Lee Sol <leeso4567@naver.com>
2022-12-28 15:28:56 +0900

    메서드 실행 시 걸린 시간 적용한 AOP 구현

---
+++ src/main/java/hello/memberservice/aop/TimeTraceAop.java
@@ -0,0 +1,24 @@
+package hello.memberservice.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* hello.memberservice..*(..))") //hello.memberservice 아래 있는 패키지의 메서드들에 모두 적용해주겠다
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
