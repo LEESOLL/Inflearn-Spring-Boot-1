6303e9bf19b6747e8b5c15497eec164eb9647547
Lee Sol <leeso4567@naver.com>
2022-12-28 13:21:39 +0900

    스프링 데이터 JPA로 설정 변경

--- src/main/java/hello/memberservice/SpringConfig.java
+++ src/main/java/hello/memberservice/SpringConfig.java
@@ -12,7 +12,7 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final MemberRepository memberRepository
+    private final MemberRepository memberRepository;
 
     @Autowired
     public SpringConfig(MemberRepository memberRepository) {
