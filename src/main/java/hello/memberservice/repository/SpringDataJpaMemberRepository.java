db06f6bf3b0f12b17dcf744eb0454728208f9a72
Lee Sol <leeso4567@naver.com>
2022-12-28 13:19:46 +0900

    스프링 데이터 JPA 구현

---
+++ src/main/java/hello/memberservice/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package hello.memberservice.repository;
+
+import hello.memberservice.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+     @Override
+    Optional<Member> findByName(String name);
+}
